2319d7ba5851733c581c6e8d41aafecf1a10a8bf
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-06-06 17:15:14 +0200

    coded the core program of the application
    used nested if to set up conditions

--- app/src/main/java/com/example/generation/Generation.kt
+++ app/src/main/java/com/example/generation/Generation.kt
@@ -1,6 +1,9 @@
 package com.example.generation
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -39,6 +42,70 @@ class Generation : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_generation)
+
+        //Declarations of all variables
+        val descripView = findViewById<TextView>(R.id.descripView)
+        val genView = findViewById<TextView>(R.id.genView)
+        val userYear = findViewById<EditText>(R.id.userYear)
+        val year = userYear.text.toString().toInt()
+        val checkBtn = findViewById<Button>(R.id.CheckBtn)
+        val resetBtn = findViewById<Button>(R.id.resetBtn)
+        resetBtn.visibility = Button.INVISIBLE
+        resetBtn.isEnabled = false
+
+        // On click listener for check button
+        checkBtn.setOnClickListener {
+            resetBtn.visibility = Button.VISIBLE
+            resetBtn.isEnabled = true
+            // settings conditions for generations
+            if (year < 1900) {
+                genView.text = arrGen[0]
+                descripView.text = arrDesc[0]
+            } else
+                if (year in 1900..<1928) {
+                    genView.text = arrGen[1]
+                    descripView.text = arrDesc[1]
+                } else
+                    if (year in 1928..<1946) {
+                        genView.text = arrGen[2]
+                        descripView.text = arrDesc[2]
+                    } else
+                        if (year in 1946..<1965) {
+                            genView.text = arrGen[3]
+                            descripView.text = arrDesc[3]
+                        } else
+                            if (year in 1965..<1981) {
+                                genView.text = arrGen[4]
+                                descripView.text = arrDesc[4]
+                            } else
+                                if (year in 1981..<1997) {
+                                    genView.text = arrGen[5]
+                                    descripView.text = arrDesc[5]
+                                } else
+                                    if (year in 1997..<2013) {
+                                        genView.text = arrGen[6]
+                                        descripView.text = arrDesc[6]
+                                    } else
+                                        if (year in 2013..<2025) {
+                                            genView.text = arrGen[7]
+                                            descripView.text = arrDesc[7]
+                                        } else
+                                            if (year in 2025..<2040) {
+                                                genView.text = arrGen[8]
+                                                descripView.text = arrDesc[8]
+                                            } else {
+                                                genView.text = arrGen[9]
+                                                descripView.text = arrDesc[9]
+                                            }
+
+
+
+            // disable check button after clicking
+            checkBtn.visibility = Button.INVISIBLE
+            checkBtn.isEnabled = false
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
